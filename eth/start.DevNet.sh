2a38cefbe3fdf6316f37003f5959cfea5a650c8e
Cyril Lapinte <cyril.lapinte@free.fr>
2018-09-09 23:13:21 +0200

    Adding DevNet

---
+++ eth/start.DevNet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT_DIR=`pwd`
+#ARGS_OPTS="-e VERBOSITY=6"
+
+echo $CURRENT_DIR
+sudo docker run -d \
+        -h eth-devnet-01 \
+        -p 30303:30303 -p 30303:30303/udp -p 8545:8545 -p 8546:8546 \
+        -v /data/mtpelerin/ethereum/eth-devnet-01:/ethereum \
+        -e DEV_MODE=true -e NETWORK_ID=20180909 $ARGS_OPTS \
+        -t mtpelerin/node-eth
+
