cfa7d85c246c2bc9da282746af8fc99fabb87145
Cyril Lapinte <cyril.lapinte@free.fr>
2018-09-23 12:13:13 +0200

    Adding scripts directory

--- eth/start.TestNet.sh
+++ eth/start.TestNet.sh
@@ -8,6 +8,7 @@ sudo docker run -d \
         -h eth-testnet-01 \
         -p 30303:30303 -p 30303:30303/udp -p 8545:8545 -p 8546:8546 \
         -v /data/mtpelerin/ethereum/eth-testnet-01:/ethereum \
+        -v ~/scripts/:/scripts \
         -e TESTNET=true $ARGS_OPTS \
         -t mtpelerin/node-eth
 
