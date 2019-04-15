28b00387ac7589921b69874a423978be87896f55
Cyril Lapinte <cyril.lapinte@free.fr>
2019-04-15 16:21:34 +0200

    Updating cache

--- eth/run.sh
+++ eth/run.sh
@@ -38,7 +38,7 @@ NODE_LOG="$DATADIR/node.log"
 GETH_OPTS="$GETH_OPTS --syncmode=full --gcmode=archive
           --rpc --rpcaddr=0.0.0.0 --rpcapi=$API_ALLOWED --rpccorsdomain='*'
           --ws --wsaddr=0.0.0.0 --wsapi=$API_ALLOWED --wsorigins=*
-          --maxpeers=$MAX_PEERS --cache=512
+          --maxpeers=$MAX_PEERS --cache=2048
           --datadir=$DATA_DIR
           --networkid=$NETWORK_ID
           --verbosity=$VERBOSITY"
