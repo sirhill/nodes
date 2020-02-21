3e761a6f35afc1c1aa087ea6ebfef44e8ca3bb68
Cyril Lapinte <cyril.lapinte@free.fr>
2020-02-21 20:55:38 +0100

    Adding IP restrictions

--- eth/run.sh
+++ eth/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Available APIs eth,miner,net,web3,personal,txpool
 echo -e "================  VARIABLES  ===================
       DATA_DIR=${DATA_DIR:="/ethereum/"}
       FREEZER_DIR=${FREEZER_DIR:="/ethereum/freezer"}
-      API_ALLOWED=${API_ALLOWED:="eth,miner,net,web3,personal,txpool"}
+      AUTHORIZED_IP=${AUTHORIZED_IP:="0.0.0.0"}
+      API_ALLOWED=${API_ALLOWED:="eth,net,web3"}
       MAX_PEERS=${MAX_PEERS:=50}
       VERBOSITY=${VERBOSITY:=1}
       TESTNET=${TESTNET:=false}
@@ -46,8 +48,8 @@ fi
 NODE_LOG="$DATA_DIR/node.log"
 
 GETH_OPTS="$GETH_OPTS --syncmode=$SYNCMODE --gcmode=$GCMODE
-          --rpc --rpcaddr=0.0.0.0 --rpcapi=$API_ALLOWED --rpccorsdomain='*'
-          --ws --wsaddr=0.0.0.0 --wsapi=$API_ALLOWED --wsorigins=*
+          --rpc --rpcaddr=$AUTHORIZED_IP --rpcapi=$API_ALLOWED --rpccorsdomain='*'
+          --ws --wsaddr=$AUTHORIZED_IP --wsapi=$API_ALLOWED --wsorigins=*
           --maxpeers=$MAX_PEERS --maxpendpeers=$MAX_PEERS --cache=$CACHE --nousb
           --lightpeers=0 --nousb
           --datadir=$DATA_DIR --datadir.ancient=$FREEZER_DIR
